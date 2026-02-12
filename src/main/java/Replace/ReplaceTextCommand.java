853f7ba3201f75a80481142883091036fbeaf5e6
Philip Mandt Broen <philibr@stud.ntnu.no>
2026-02-12 14:16:04 +0100

    v1.0.0 ReplaceTextCommand startet creating class

--- src/main/java/Replace/ReplaceTextCommand.java
+++ src/main/java/Replace/ReplaceTextCommand.java
@@ -1,4 +1,13 @@
 package Replace;
 
-public class ReplaceTextCommand {
+public class ReplaceTextCommand implements TextCommand{
+
+    private String target;
+    private String replacement;
+
+    public ReplaceTextCommand(String target, String replacement) {
+        this.target = target;
+        this.replacement = replacement;
+    }
+
 }
