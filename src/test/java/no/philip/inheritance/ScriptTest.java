3decdb2301f76b57f5f65d650b7ab99a4884adbc
Philip Mandt Broen <philibr@stud.ntnu.no>
2026-02-23 09:06:15 +0100

    Final commit

--- src/test/java/no/philip/inheritance/ScriptTest.java
+++ src/test/java/no/philip/inheritance/ScriptTest.java
@@ -1,7 +1,35 @@
 package no.philip.inheritance;
 
+import no.philip.inheritance.capitalize.CapitalizeTextCommand;
+import no.philip.inheritance.replace.ReplaceTextCommand;
+import no.philip.inheritance.wrap.WrapTextCommand;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScriptTest {
 
+    @Test
+    void testSingleCommand() {
+        Script script = new Script(List.of(new CapitalizeTextCommand()));
+        assertEquals("Hello there", script.execute("hello there"));
+        System.out.println(script.execute("hello there"));
+    }
+
+    @Test
+    void testMultipleCommandsInOder() {
+        Script script = new Script(List.of(new CapitalizeTextCommand(), new WrapTextCommand("<p>", "</p>")));
+        assertEquals("<p>Hello there</p>", script.execute("hello there"));
+        System.out.println(script.execute("hello there"));
+    }
+
+    @Test
+    void testEmptyCommandList() {
+        Script script = new Script(List.of());
+        assertEquals("hello there", script.execute("hello there"));
+        System.out.printf(script.execute("hello there"));
+    }
+    
 }
